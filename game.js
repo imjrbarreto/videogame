f8c2654c755d3dbf2ecd65ed4152048f046efe04
imjrbarreto <imjrbarreto@gmail.com>
2024-02-05 11:08:26 -0300

    canvas JS

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-console.log(maps)
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+// Start of the game:
+window.addEventListener('load', startGame);
+
+
+function startGame() {
+  // game.fillRect(0,0,100,100);
+  // game.clearRect(0,0,50,50);
+  // game.clearRect(50,50,100,100);
+
+  game.font = '25px Verdana';
+  game.fillStyle = "purple";
+  game.textAlign = 'start'
+  game.fillText('Platzi', 100, 100);
+}
+
