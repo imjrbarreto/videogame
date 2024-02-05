2a110f903e3dc69df96015111aab22545de23ffb
imjrbarreto <imjrbarreto@gmail.com>
2024-02-05 11:32:32 -0300

    Size of the canvas and elements

--- game.js
+++ game.js
@@ -9,13 +9,40 @@ window.addEventListener('load', startGame);
 
 
 function startGame() {
+  let canvasSize;
+
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8
+  } else {
+    canvasSize = window.innerHeight * 0.8
+  }
+
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+
+  const elementsSize = canvasSize/10;
+
+  console.log(canvasSize);
+  console.log(elementsSize);
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let index = 1; index <= 10; index++) {
+    game.fillText(emojis['X'], elementsSize * index, elementsSize);
+  }
+
+
+  // window.innerWidth
+  // window.innerHeight
+
   // game.fillRect(0,0,100,100);
   // game.clearRect(0,0,50,50);
   // game.clearRect(50,50,100,100);
 
-  game.font = '25px Verdana';
-  game.fillStyle = "purple";
-  game.textAlign = 'start'
-  game.fillText('Platzi', 100, 100);
+  // game.font = '25px Verdana';
+  // game.fillStyle = "purple";
+  // game.textAlign = 'start'
+  // game.fillText('Platzi', 100, 100);
 }
 
